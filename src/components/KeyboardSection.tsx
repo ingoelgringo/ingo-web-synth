908b872d2f6c5525f743b5f0314c416950ea4276
Ingemar Berghult <ingemarberghult@Ingemars-MacBook-Air.local>
2026-02-21 15:26:52 +0100

    keyboard styling

--- src/components/KeyboardSection.tsx
+++ src/components/KeyboardSection.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "../KeyboardStyles.css"; // Import the new style
 
 export const KeyboardSection: React.FC = () => {
